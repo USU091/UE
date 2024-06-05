41fcb4f8129482866419c33c874d9e2048d642a3
ysy <boneoflily@naver.com>
2024-06-05 09:03:55 +0900

    Input
    1. InputAction = ActionManager + AxisMapping
    2. 실제로 키보드와 InputAction들을 매핑시켜주는것은 InputMappingContext임
    3.시작 =Start, 누르는거 = Triggered, 끝 = Completed

--- R1/Source/R1/Player/R1PlayerController.h
+++ R1/Source/R1/Player/R1PlayerController.h
@@ -1,10 +1,12 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
 #include "R1PlayerController.generated.h"
+
+struct FInputActionValue;
 
 /**
  * 
@@ -13,5 +15,37 @@ UCLASS()
 class R1_API AR1PlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+
+
+public:
+	AR1PlayerController(const FObjectInitializer& ObjectInitializer);
+
+private:
+	//Pawn도 Actor를 상속받았기 때문에 BeginPlay(), SetupInputComponent() 를 override해 올 것임
+
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+
+private:
+	void Input_Test(const FInputActionValue& InputValue);
+	void Input_Move(const FInputActionValue& InputValue);
+	void Input_Turn(const FInputActionValue& InputValue);
+
+protected:
+	UPROPERTY(Category=Input, EditAnywhere)
+	TObjectPtr<class UInputMappingContext> InputMappingContext;
+
+	UPROPERTY(Category = Input, EditAnywhere)
+	TObjectPtr<class UInputAction> TestAction;
+
+	UPROPERTY(Category = Input, EditAnywhere)
+	TObjectPtr<class UInputAction> MoveAction;
+
+
+	UPROPERTY(Category = Input, EditAnywhere)
+	TObjectPtr<class UInputAction> TurnAction;
+
+
+
+
 };
--- R1/Source/R1/R1.Build.cs
+++ R1/Source/R1/R1.Build.cs
@@ -13,7 +13,8 @@ public class R1 : ModuleRules
 			"R1"
 		});
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine",
+			"InputCore", "EnhancedInput" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
